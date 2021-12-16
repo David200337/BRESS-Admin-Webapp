80abb7501b4177e5b671bcb065a8e092729c556c
stefandenijs <shlg.denijs@student.avans.nl>
2021-12-16 15:55:55 +0100

    Created a functional bracket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,79 +16,50 @@ export class AppComponent {
         matches: [
           {
             teams: [
-              { name: 'Team  A', score: 1 },
-              { name: 'Team  B', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
           {
             teams: [
-              { name: 'Team  C', score: 1 },
-              { name: 'Team  D', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
           {
             teams: [
-              { name: 'Team  E', score: 1 },
-              { name: 'Team  F', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
           {
             teams: [
-              { name: 'Team  G', score: 1 },
-              { name: 'Team  H', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
           {
             teams: [
-              { name: 'Team  A', score: 1 },
-              { name: 'Team  B', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
           {
             teams: [
-              { name: 'Team  C', score: 1 },
-              { name: 'Team  D', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
           {
             teams: [
-              { name: 'Team  E', score: 1 },
-              { name: 'Team  F', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
           {
             teams: [
-              { name: 'Team  G', score: 1 },
-              { name: 'Team  H', score: 2 },
-            ],
-          },
-        ],
-      },
-      {
-        type: 'Winnerbracket',
-        matches: [
-          {
-            teams: [
-              { name: 'Team  A', score: 1 },
-              { name: 'Team  B', score: 2 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Team  C', score: 1 },
-              { name: 'Team  D', score: 2 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Team  E', score: 1 },
-              { name: 'Team  F', score: 2 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Team  G', score: 1 },
-              { name: 'Team  H', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
         ],
@@ -98,14 +69,43 @@ export class AppComponent {
         matches: [
           {
             teams: [
-              { name: 'Team  B', score: 1 },
-              { name: 'Team  D', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
           {
             teams: [
-              { name: 'Team  F', score: 1 },
-              { name: 'Team  H', score: 2 },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
+            ],
+          },
+          {
+            teams: [
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
+            ],
+          },
+          {
+            teams: [
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
+            ],
+          },
+        ],
+      },
+      {
+        type: 'Winnerbracket',
+        matches: [
+          {
+            teams: [
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
+            ],
+          },
+          {
+            teams: [
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
         ],
@@ -115,14 +115,8 @@ export class AppComponent {
         matches: [
           {
             teams: [
-              {
-                name: 'Team  D',
-                score: 1,
-              },
-              {
-                name: 'Team  H',
-                score: 2,
-              },
+              { name: 'Robin Schellius', score: 1 },
+              { name: 'Robin Schellius', score: 2 },
             ],
           },
         ],
