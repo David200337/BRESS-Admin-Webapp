4942f00da9b1b06e7cf7a0fa078f3133420a3b9b
FSGabrsek <f.gabrsek@gmail.com>
2021-12-16 14:45:45 +0100

    fixed login page with route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/views/dashboard/dashboard.component';
+import { LoginComponent } from './views/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "login" },
+  { path: "login", pathMatch: "full", component: LoginComponent },
+  { path: "dashboard", pathMatch: "full", component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
