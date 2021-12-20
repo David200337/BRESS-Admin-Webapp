a55efed4f9c3b4e1f7ee47a7907f756915208a39
David van Mourik <dcwvanmourik@gmail.com>
2021-12-20 14:36:40 +0100

    Remove default attribute initialization

--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -1,8 +1,8 @@
 import { Round } from "./round.model";
 
 export class Category {
-	public id: number = -1;
-	public name: string = "";
+	public id: number;
+	public name: string;
 	public rounds: Round[];
 
 	constructor(id: number, name: string, rounds: Round[]) {
