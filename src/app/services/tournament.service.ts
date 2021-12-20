efe9bbface264000d06cad46987e3d24b0a9d25d
David van Mourik <dcwvanmourik@gmail.com>
2021-12-20 17:33:10 +0100

    Add method for getting all categories from tournament

--- src/app/services/tournament.service.ts
+++ src/app/services/tournament.service.ts
@@ -1,13 +1,25 @@
 import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Category } from "../models/category.model";
 import { Tournament } from "../models/tournament.model";
 import { ResourceService } from "./resource.service";
 
+@Injectable({
+	providedIn: "root"
+})
 export class TournamentService extends ResourceService<Tournament> {
+    // private readonly APIUrl = environment.apiUrl + this.getResourceUrl();
+
     constructor(protected override httpClient: HttpClient) {
         super(httpClient);
     }
 
     public getResourceUrl(): string {
         return "/tournament";
+    }
+
+    public getAllCategories(id: number): Observable<Category> {
+        return this.httpClient.get<Category>(`${this.APIUrl}/${id}/category`);
     }
 }
