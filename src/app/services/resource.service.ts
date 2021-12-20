8a12a0957b084d40a16764931b9d7e81372a525f
David van Mourik <dcwvanmourik@gmail.com>
2021-12-20 16:25:09 +0100

    Fix wrong api url being called

--- src/app/services/resource.service.ts
+++ src/app/services/resource.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, Observable, throwError } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
 	providedIn: "root"
 })
 export abstract class ResourceService<T> {
-	private readonly APIUrl = process.env.API_URL + this.getResourceUrl();
+	private readonly APIUrl = environment.apiUrl + this.getResourceUrl();
 
 	constructor(protected httpClient: HttpClient) {}
 
@@ -14,36 +15,38 @@ export abstract class ResourceService<T> {
 
 	public getList(): Observable<T[]> {
 		return this.httpClient
-			.get<T[]>(`/${this.APIUrl}`)
+			.get<T[]>(`${this.APIUrl}`)
 			.pipe(catchError(this.handleError));
 	}
 
 	public get(id: number): Observable<T> {
 		return this.httpClient
-			.get<T>(`/${this.APIUrl}/${id}`)
+			.get<T>(`${this.APIUrl}/${id}`)
 			.pipe(catchError(this.handleError));
 	}
 
 	public add(resource: T): Observable<any> {
 		return this.httpClient
-			.post(`/${this.APIUrl}`, resource)
+			.post(`${this.APIUrl}`, resource)
 			.pipe(catchError(this.handleError));
 	}
 
 	public delete(id: number): Observable<any> {
 		return this.httpClient
-			.delete(`/${this.APIUrl}/${id}`)
+			.delete(`${this.APIUrl}/${id}`)
 			.pipe(catchError(this.handleError));
 	}
 
 	public update(resource: T): Observable<any> {
 		return this.httpClient
-			.put(`/${this.APIUrl}`, resource)
+			.put(`${this.APIUrl}`, resource)
 			.pipe(catchError(this.handleError));
 	}
 
 	private handleError(error: HttpErrorResponse) {
 		// Handle HTTP errors
+		console.log(error);
+
 		return throwError(() => new Error("HTTP Request failed."));
 	}
 }
