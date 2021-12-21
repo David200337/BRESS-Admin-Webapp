4059dc37f8b514eb3073002731f98f5326f41029
stefandenijs <shlg.denijs@student.avans.nl>
2021-12-21 16:11:40 +0100

    Connected create tournament to tournament service, yet to be tested

--- src/app/admin/views/create-tournament/create-tournament.component.ts
+++ src/app/admin/views/create-tournament/create-tournament.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Tournament } from 'src/app/models/tournament.model';
+import { TournamentService } from 'src/app/services/tournament.service';
 
 @Component({
   selector: 'app-create-tournament',
@@ -9,7 +12,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CreateTournamentComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private router: Router,
+    private tournamentService: TournamentService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -18,5 +25,32 @@ export class CreateTournamentComponent implements OnInit {
       maxPlayers: ['', Validators.required],
       entryFee: ['', Validators.required],
     });
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      const tournament = new Tournament(
+        -1,
+        this.form.value.title,
+        this.form.value.beginDateTime,
+        this.form.value.entryFee,
+        this.form.value.maxPlayer,
+        0,
+        0,
+        [],
+        []
+      );
+      this.tournamentService.add(tournament).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    } else {
+      return;
+    }
   }
 }
