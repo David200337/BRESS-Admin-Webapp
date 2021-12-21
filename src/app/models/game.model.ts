330bc44d22e36a0f65130d4b362c6f957b4e1e7a
David van Mourik <dcwvanmourik@gmail.com>
2021-12-21 12:41:22 +0100

    Update game model

--- src/app/models/game.model.ts
+++ src/app/models/game.model.ts
@@ -6,6 +6,7 @@ export class Game extends GenericModel {
 	public score: string;
 	public winner: number;
 	public inQueue: boolean;
+    public gameStarted: boolean;
 	public field: Field;
 	public player1: Player;
 	public player2: Player;
@@ -15,6 +16,7 @@ export class Game extends GenericModel {
 		score: string,
 		winner: number,
 		inQueue: boolean,
+        gameStarted: boolean,
 		field: Field,
 		player1: Player,
 		player2: Player
@@ -23,6 +25,7 @@ export class Game extends GenericModel {
 		this.score = score;
 		this.winner = winner;
 		this.inQueue = inQueue;
+        this.gameStarted = gameStarted;
 		this.field = field;
 		this.player1 = player1;
 		this.player2 = player2;
