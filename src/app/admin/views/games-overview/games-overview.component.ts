58f4159ce065973f4a77629fe1ca89f5d4b79559
FSGabrsek <f.gabrsek@gmail.com>
2021-12-21 15:02:09 +0100

    implemented sorting function

--- src/app/admin/views/games-overview/games-overview.component.ts
+++ src/app/admin/views/games-overview/games-overview.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Field } from 'src/app/models/field.model';
 import { Game } from 'src/app/models/game.model';
+import { Player } from 'src/app/models/player.model';
+import { SkillLevel } from 'src/app/models/skillLevel.model';
 
 @Component({
   selector: 'app-games-overview',
@@ -15,6 +18,16 @@ export class GamesOverviewComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.games = new Array();
+    this.activeGames = new Array();
+    this.futureGames = new Array();
+    for (let index = 0; index < 5; index++) {
+      this.games.push(new Game(0, "0 - 0", 0, true, true, new Field(0, "test field", true), new Player(0, "name1", "email", new SkillLevel(0, "level")), new Player(0, "name2", "email", new SkillLevel(0, "level"))))
+      this.games.push(new Game(0, "0 - 0", 0, true, false, undefined, new Player(0, "name1", "email", new SkillLevel(0, "level")), new Player(0, "name2", "email", new SkillLevel(0, "level"))))
+    }
+    this.sortGames(this.games);
+    console.log(this.activeGames);
+    
   }
 
   /**
@@ -24,7 +37,7 @@ export class GamesOverviewComponent implements OnInit {
    */
   sortGames(games: Game[]) {
     games.forEach(g => {
-      if (true /* g.gameStarted */ ) {
+      if (g.gameStarted) {
         this.activeGames.push(g)
       } else {
         this.futureGames.push(g)
