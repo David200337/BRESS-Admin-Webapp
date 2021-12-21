e2aa4c1c6e0b49c56503158800af3dbccf0282cf
David van Mourik <dcwvanmourik@gmail.com>
2021-12-21 09:53:25 +0100

    Add getAllPools method

--- src/app/services/tournament.service.ts
+++ src/app/services/tournament.service.ts
@@ -10,19 +10,32 @@ import { ResourceService } from "./resource.service";
 	providedIn: "root"
 })
 export class TournamentService extends ResourceService<Tournament> {
-    constructor(protected override httpClient: HttpClient) {
-        super(httpClient);
-    }
+	constructor(protected override httpClient: HttpClient) {
+		super(httpClient);
+	}
 
-    public getResourceUrl(): string {
-        return "/tournament";
-    }
+	public getResourceUrl(): string {
+		return "/tournament";
+	}
 
-    public getAllCategories(tournamentId: number): Observable<Category> {
-        return this.httpClient.get<Category>(`${this.APIUrl}/${tournamentId}/category`);
-    }
+	public getAllCategories(tournamentId: number): Observable<Category> {
+		return this.httpClient.get<Category>(
+			`${this.APIUrl}/${tournamentId}/category`
+		);
+	}
 
-    public getCategory(tournamentId: number): Observable<Category> {
-        return this.httpClient.get<Category>(`${this.APIUrl}/${tournamentId}/category`)
-    }
+	public getCategory(tournamentId: number): Observable<Category> {
+		return this.httpClient.get<Category>(
+			`${this.APIUrl}/${tournamentId}/category`
+		);
+	}
+
+	public getAllPools(
+		tournamentId: number,
+		categoryId: number
+	): Observable<Pool> {
+		return this.httpClient.get<Pool>(
+			`${this.APIUrl}/${tournamentId}/category/${categoryId}/pool`
+		);
+	}
 }
