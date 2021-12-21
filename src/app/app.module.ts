f714f3e76c8cc77e1dc4b26810fe4a40ce62e1da
David van Mourik <dcwvanmourik@gmail.com>
2021-12-21 12:30:56 +0100

    Fix interceptor not being used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AdminModule } from './admin/admin.module';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AdminModule } from "./admin/admin.module";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./core/navbar/navbar.component";
 
-import { LoginComponent } from './views/login/login.component';
-import { RegisterComponent } from './views/register/register.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from "./views/login/login.component";
+import { RegisterComponent } from "./views/register/register.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { TokenInterceptor } from "./services/token.interceptor";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    LoginComponent,
-    RegisterComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AdminModule,
-    HttpClientModule,
-    ReactiveFormsModule
-  ],
-  exports: [
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavbarComponent,
+		LoginComponent,
+		RegisterComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		AdminModule,
+		HttpClientModule,
+		ReactiveFormsModule
+	],
+	exports: [],
+	providers: [
+		{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
