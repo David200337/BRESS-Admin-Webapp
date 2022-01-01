971330359e4b5e4ce5292ccb2ac8811152840268
qloozen <qiangloozen@gmail.com>
2022-01-01 17:00:47 +0100

    Created/Applied basic auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { PlayerDetailComponent } from './admin/views/player/player-detail/player
 import { PoolDetailComponent } from './admin/views/pool-detail/pool-detail.component';
 import { TournamentDetailComponent } from './admin/views/tournament-detail/tournament-detail.component';
 import { TournamentOverviewComponent } from './admin/views/tournament-overview/tournament-overview.component';
+import { AuthGuard } from './authentication/auth.guard';
 import { LoaderComponent } from './shared/loader/loader.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
@@ -21,18 +22,18 @@ const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "login" },
   { path: "login", pathMatch: "full", component: LoginComponent },
   { path: "register", pathMatch: "full", component: RegisterComponent },
-  { path: "dashboard", pathMatch: "full", component: DashboardComponent },
-  { path: "create-tournament", pathMatch: "full", component: CreateTournamentComponent },
-  { path: "tournament/:id", pathMatch: "full", component: TournamentOverviewComponent },
-  { path: "tournament/:id/games", pathMatch: "full", component: GamesOverviewComponent },
-  { path: "tournament/:id/score-overview", pathMatch: "full", component: TournamentDetailComponent },
-  { path: "tournament/:id/edit-tournament", pathMatch: "full", component: EditTournamentComponent },
-  { path: "tournament/:id/score-overview/:categoryId/:poolId", pathMatch: "full", component: PoolDetailComponent },
-  { path: "tournament/:tournamentId/bracket", pathMatch: "full", component: CategoryBracketComponent },
-  { path: "players/create", pathMatch: "full", component: CreatePlayerComponent },
-  { path: "players/:id/edit", pathMatch: "full", component: EditPlayerComponent },
+  { path: "dashboard", pathMatch: "full", canActivate: [AuthGuard], component: DashboardComponent },
+  { path: "create-tournament", pathMatch: "full", canActivate: [AuthGuard], component: CreateTournamentComponent },
+  { path: "tournament/:id", pathMatch: "full", canActivate: [AuthGuard], component: TournamentOverviewComponent },
+  { path: "tournament/:id/games", pathMatch: "full", canActivate: [AuthGuard], component: GamesOverviewComponent },
+  { path: "tournament/:id/score-overview", pathMatch: "full", canActivate: [AuthGuard], component: TournamentDetailComponent },
+  { path: "tournament/:id/edit-tournament", pathMatch: "full", canActivate: [AuthGuard], component: EditTournamentComponent },
+  { path: "tournament/:id/score-overview/:categoryId/:poolId", pathMatch: "full", canActivate: [AuthGuard], component: PoolDetailComponent },
+  { path: "tournament/:tournamentId/bracket", pathMatch: "full", canActivate: [AuthGuard], component: CategoryBracketComponent },
+  { path: "players/create", pathMatch: "full", canActivate: [AuthGuard], component: CreatePlayerComponent },
+  { path: "players/:id/edit", pathMatch: "full", canActivate: [AuthGuard], component: EditPlayerComponent },
   { path: "svg", pathMatch: "full", component: LoaderComponent },
-  { path: "create-skilllevel", pathMatch: "full", component: CreateSkilllevelComponent },
+  { path: "create-skilllevel", pathMatch: "full", canActivate: [AuthGuard], component: CreateSkilllevelComponent },
   {
     path: "players",
     component: PlayerColumnComponent,
