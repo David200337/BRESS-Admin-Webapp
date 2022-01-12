997cba6c6264ff07f0bf13ca76ced85253a44d12
David van Mourik <dcwvanmourik@gmail.com>
2022-01-12 14:26:05 +0100

    Add create player test

--- src/app/admin/views/player/create-player/create-player.component.spec.ts
+++ src/app/admin/views/player/create-player/create-player.component.spec.ts
@@ -1,30 +1,122 @@
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from "@angular/common/http";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
 
-import { CreatePlayerComponent } from './create-player.component';
+import { CreatePlayerComponent } from "./create-player.component";
 
-describe('CreatePlayerComponent', () => {
-  let component: CreatePlayerComponent;
-  let fixture: ComponentFixture<CreatePlayerComponent>;
+describe("CreatePlayerComponent", () => {
+	let component: CreatePlayerComponent;
+	let fixture: ComponentFixture<CreatePlayerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ HttpClientModule, ReactiveFormsModule, RouterTestingModule ],
-      providers: [ FormBuilder ],
-      declarations: [ CreatePlayerComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [
+				HttpClientModule,
+				ReactiveFormsModule,
+				RouterTestingModule
+			],
+			providers: [FormBuilder],
+			declarations: [CreatePlayerComponent]
+		}).compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreatePlayerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(CreatePlayerComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
+
+	it("should contain the same number of ui input elements as reactiveFormControls", () => {
+		const formElement =
+			fixture.debugElement.nativeElement.querySelector("form");
+		const inputElements = formElement.querySelectorAll("input");
+		const selectElements = formElement.querySelectorAll("select");
+
+		expect(inputElements.length).toEqual(2);
+		expect(selectElements.length).toEqual(1);
+	});
+
+	it("should check if formGroup is correct", () => {
+		const playerFromGroup = component.form;
+		const playerFormValues = {
+			name: "",
+			email: "",
+			skillLevel: ""
+		};
+
+		expect(playerFromGroup.value).toEqual(playerFormValues);
+	});
+
+	it("should check if formControls required validation is correct", () => {
+		const inputElements: HTMLInputElement[] =
+			fixture.debugElement.nativeElement
+				.querySelector("form")
+				.querySelectorAll("input");
+
+		const playerNameInputElement: HTMLInputElement = inputElements[0];
+		const playerNameValue = component.form.get("name");
+		expect(playerNameInputElement.value).toEqual(playerNameValue?.value);
+		expect(playerNameValue?.errors).not.toBeNull();
+		expect(playerNameValue?.errors).toBeTruthy();
+
+		const playerEmailInputElement: HTMLInputElement = inputElements[1];
+		const playerEmailValue = component.form.get("email");
+		expect(playerEmailInputElement.value).toEqual(playerEmailValue?.value);
+		expect(playerEmailValue?.errors).not.toBeNull();
+		expect(playerEmailValue?.errors).toBeTruthy();
+
+		const skillLevelSelectElement: HTMLSelectElement =
+			fixture.debugElement.nativeElement
+				.querySelector("form")
+				.querySelectorAll("select")[0];
+		const skillLevelSelectValue = component.form.get("skillLevel");
+		expect(skillLevelSelectElement.value).toEqual(
+			skillLevelSelectValue?.value
+		);
+		expect(skillLevelSelectValue?.errors).not.toBeNull();
+		expect(skillLevelSelectValue?.errors).toBeTruthy();
+	});
+
+	it("should check if the form is valid", () => {
+		const inputElements: HTMLInputElement[] =
+			fixture.debugElement.nativeElement
+				.querySelector("form")
+				.querySelectorAll("input");
+		const selectElements: HTMLSelectElement[] =
+			fixture.debugElement.nativeElement
+				.querySelector("form")
+				.querySelectorAll("select");
+
+		const playerNameInputElement = inputElements[0];
+		playerNameInputElement.value = "Speler 1";
+		playerNameInputElement.dispatchEvent(new Event("input"));
+		fixture.detectChanges();
+		fixture.whenStable().then(() => {
+			const isFormValid = component.form.valid;
+			expect(isFormValid).toBeTruthy;
+		});
+
+		const playerEmailInputElement = inputElements[0];
+		playerEmailInputElement.value = "speler@email.com";
+		playerEmailInputElement.dispatchEvent(new Event("input"));
+		fixture.detectChanges();
+		fixture.whenStable().then(() => {
+			const isFormValid = component.form.valid;
+			expect(isFormValid).toBeTruthy;
+		});
+
+		const skillLevelSelectElement: HTMLSelectElement = selectElements[0];
+        skillLevelSelectElement.value = "Beginners";
+		skillLevelSelectElement.dispatchEvent(new Event("select"));
+		fixture.detectChanges();
+		fixture.whenStable().then(() => {
+			const isFormValid = component.form.valid;
+			expect(isFormValid).toBeTruthy;
+		});
+	});
 });
