3903482a54c6d4be69394ffbc1153abc79da8713
stefandenijs <shlg.denijs@student.avans.nl>
2022-01-14 11:50:55 +0100

    Basic login tests Added reusable intercept commands

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -15,3 +15,64 @@
 
 // When a command from ./commands is ready to use, import with `import './commands'` syntax
 // import './commands';
+
+declare namespace Cypress {
+  interface Chainable<Subject = any> {
+    userLogin(): Chainable<null>;
+  }
+  interface Chainable<Subject = any> {
+    getTournaments(): Chainable<null>;
+  }
+  interface Chainable<Subject = any> {
+    getPlayers(): Chainable<null>;
+  }
+  interface Chainable<Subject = any> {
+    getFields(): Chainable<null>;
+  }
+}
+
+Cypress.Commands.add('userLogin', () => {
+  cy.intercept(
+    { method: 'POST', url: '**/api/login' },
+    {
+      statusCode: 200,
+      body: {
+        result: {
+          token: 'token',
+          expireDate: new Date().setHours(+24),
+          user: 'j.doe@email.nl',
+        },
+      },
+    }
+  ).as('userLogin');
+});
+
+Cypress.Commands.add('getTournaments', () => {
+  cy.intercept(
+    { method: 'GET', url: '**/api/tournament' },
+    {
+      statusCode: 200,
+      fixture: 'tournaments.json',
+    }
+  ).as('getTournaments');
+});
+
+Cypress.Commands.add('getPlayers', () => {
+    cy.intercept(
+      { method: 'GET', url: '**/api/player' },
+      {
+        statusCode: 200,
+        fixture: 'players.json',
+      }
+    ).as('getPlayers');
+  });
+
+Cypress.Commands.add('getFields', () => {
+    cy.intercept(
+      { method: 'GET', url: '**/api/Field' },
+      {
+        statusCode: 200,
+        fixture: 'fields.json',
+      }
+    ).as('getFields');
+  });
