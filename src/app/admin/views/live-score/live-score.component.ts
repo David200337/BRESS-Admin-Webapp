c90d74049d20787a8b1bb7b3158a37d234fe0173
basbuijsen <bbuijsen@gmail.com>
2022-01-17 13:33:26 +0100

    fixed bracket and full name

--- src/app/admin/views/live-score/live-score.component.ts
+++ src/app/admin/views/live-score/live-score.component.ts
@@ -11,17 +11,17 @@ import { TournamentService } from 'src/app/services/tournament.service';
 import { TournamentInterface } from 'src/app/shared/tournament-bracket/declarations/interfaces';
 import { CategorySelectorComponent } from '../../shared/category-selector/category-selector.component';
 
-
 @Component({
   selector: 'app-live-score',
   templateUrl: './live-score.component.html',
-  styleUrls: ['./live-score.component.scss']
+  styleUrls: ['./live-score.component.scss'],
 })
 export class LiveScoreComponent implements OnInit, AfterViewInit {
-  @ViewChild(CategorySelectorComponent) categorySelector!: CategorySelectorComponent;
+  @ViewChild(CategorySelectorComponent)
+  categorySelector!: CategorySelectorComponent;
 
   tournamentId!: number;
-  tournament!: Tournament
+  tournament!: Tournament;
   games!: Game[];
   activeGames!: Game[];
   futureGames!: Game[];
@@ -37,125 +37,7 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
   selectedCategoryIndex: number = 0;
   hasFinales: Boolean = false;
 
-  myTournamentData: TournamentInterface = {
-    rounds: [
-      {
-        type: 'Winnerbracket',
-        name: "",
-        matches: [
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-            id: 0
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-        ],
-      },
-      {
-        type: 'Winnerbracket',
-        name: "",
-        matches: [
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-        ],
-      },
-      {
-        type: 'Winnerbracket',
-        name: "",
-        matches: [
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-        ],
-      },
-      {
-        type: 'Final',
-        name: "",
-        matches: [
-          {
-            teams: [
-              { name: 'Not determined', score: 0 },
-              { name: 'Not determined', score: 0 },
-            ],
-          },
-        ],
-      },
-    ],
-  };
+  myTournamentData: TournamentInterface = { rounds: [] };
 
   constructor(
     private tournamentService: TournamentService,
@@ -168,7 +50,7 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
     loaderToggle.loaderVisible();
   }
   ngAfterViewInit(): void {
-    this.categorySelector.selectCategoryByIndex(this.selectedCategoryIndex)
+    this.categorySelector.selectCategoryByIndex(this.selectedCategoryIndex);
   }
 
   ngOnInit(): void {
@@ -178,44 +60,174 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
       this.tournamentId = params['id'];
     });
 
+    this.setDefaultBracket();
+
     this.editGameService.tournamentId = this.tournamentId;
 
-    this.tournamentService.get(this.tournamentId)
-      .subscribe(res => this.tournament = res)
+    this.tournamentService
+      .get(this.tournamentId)
+      .subscribe((res) => (this.tournament = res));
 
     concat(
       this.tournamentService.getPoolQueue(this.tournamentId),
       this.tournamentService.getFinaleQueue(this.tournamentId)
-    ).pipe(
-      tap(g => gamesList.push(...g)),
-      tap(() => this.sortGames(gamesList)),
-    ).subscribe()
+    )
+      .pipe(
+        tap((g) => gamesList.push(...g)),
+        tap(() => this.sortGames(gamesList))
+      )
+      .subscribe()
       .add(() => {
         this.games = gamesList;
       });
 
-    this.hideGameEdit()
+    this.hideGameEdit();
     this.tournamentService.get(this.tournamentId).subscribe((result: any) => {
       this.tournament = result;
       this.categoryList = this.tournament.categories;
-      this.editGame.tournamentId = this.tournament.id
-      this.createBracket(this.tournament.categories[this.selectedCategoryIndex].id);
-      this.startRefresh()
+      this.editGame.tournamentId = this.tournament.id;
+      this.createBracket(
+        this.tournament.categories[this.selectedCategoryIndex].id
+      );
+      this.startRefresh();
     });
   }
 
   ngOnDestroy(): void {
-    clearInterval(this.interval)
+    clearInterval(this.interval);
+  }
+
+  setDefaultBracket() {
+    this.myTournamentData = {
+      rounds: [
+        {
+          type: 'Winnerbracket',
+          name: '',
+          matches: [
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+              id: 0,
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'Winnerbracket',
+          name: '',
+          matches: [
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'Winnerbracket',
+          name: '',
+          matches: [
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'Final',
+          name: '',
+          matches: [
+            {
+              teams: [
+                { name: 'Not determined', score: 0 },
+                { name: 'Not determined', score: 0 },
+              ],
+            },
+          ],
+        },
+      ],
+    };
   }
 
   startRefresh() {
     this.interval = setInterval(() => {
+      this.setDefaultBracket();
       this.selectedCategoryIndex++;
       if (this.selectedCategoryIndex > 2) {
         this.selectedCategoryIndex = 0;
       }
-      this.categorySelector.selectCategoryByIndex(this.selectedCategoryIndex)
-      this.refreshGames()
+      this.categorySelector.selectCategoryByIndex(this.selectedCategoryIndex);
+      this.refreshGames();
       this.refreshData();
     }, 10000);
   }
@@ -226,11 +238,10 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
    * sort the provided games array into started games, the next game and all future games
    */
   sortGames(games: Game[]) {
-
     let sortedActive: Game[] = [];
     let sortedFuture: Game[] = [];
 
-    games.forEach(g => {
+    games.forEach((g) => {
       if (!g.score) {
         if (g.field) {
           sortedActive.push(g);
@@ -257,18 +268,21 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
     concat(
       this.tournamentService.getPoolQueue(this.tournamentId),
       this.tournamentService.getFinaleQueue(this.tournamentId)
-    ).pipe(
-      tap(g => gameList.push(...g))
-    ).subscribe().add(() => {
-      this.sortGames(gameList)
-    })
+    )
+      .pipe(tap((g) => gameList.push(...g)))
+      .subscribe()
+      .add(() => {
+        this.sortGames(gameList);
+      });
   }
 
   refreshData() {
     this.tournamentService.get(this.tournament.id).subscribe((result) => {
       this.tournament = result;
-      this.createBracket(this.tournament.categories[this.selectedCategoryIndex].id);
-    })
+      this.createBracket(
+        this.tournament.categories[this.selectedCategoryIndex].id
+      );
+    });
   }
 
   createBracket(categoryId: number): void {
@@ -277,28 +291,44 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
       if (cat.id == categoryId) {
         category = cat;
       }
-    })
+    });
 
     let hasFinales = false;
     category.rounds.forEach((item: any) => {
       if (!item.isPoolRound) {
         hasFinales = true;
         this.myTournamentData.rounds.forEach((round: any) => {
-
           if (item.games.length === round.matches.length) {
             let gameList: any = [];
 
             for (let i = 0; i < round.matches.length; i++) {
               const game = item.games[i];
-              const scorePlayer1: number = game.score == null ? 0 : game.score.split(" - ")[0];
-              const scorePlayer2: number = game.score == null ? 0 : game.score.split(" - ")[1];
-              const gamePlayed: boolean = scorePlayer1 != 0 || scorePlayer2 != 0;
-              gameList.push({ teams: [{ name: game.player1.firstName, score: scorePlayer1, gamePlayed }, { name: game.player2.firstName, score: scorePlayer2, gamePlayed }], id: game.id });
+              const scorePlayer1: number =
+                game.score == null ? 0 : game.score.split(' - ')[0];
+              const scorePlayer2: number =
+                game.score == null ? 0 : game.score.split(' - ')[1];
+              const gamePlayed: boolean =
+                scorePlayer1 != 0 || scorePlayer2 != 0;
+              gameList.push({
+                teams: [
+                  {
+                    name: game.player1.firstName + ' ' + game.player1.lastName,
+                    score: scorePlayer1,
+                    gamePlayed,
+                  },
+                  {
+                    name: game.player2.firstName + ' ' + game.player2.lastName,
+                    score: scorePlayer2,
+                    gamePlayed,
+                  },
+                ],
+                id: game.id,
+              });
             }
 
             round.matches = gameList;
           }
-        })
+        });
       }
     });
 
@@ -306,7 +336,10 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
       let newlist = [];
       let isFirst: boolean = true;
       for (let i = 0; i < this.myTournamentData.rounds.length; i++) {
-        if (this.myTournamentData.rounds[i].matches[0].teams[0].name != 'Not determined') {
+        if (
+          this.myTournamentData.rounds[i].matches[0].teams[0].name !=
+          'Not determined'
+        ) {
           newlist.push(this.myTournamentData.rounds[i]);
           isFirst = false;
         } else if (!isFirst) {
@@ -315,7 +348,10 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
       }
       this.myTournamentData = { rounds: newlist };
     }
-    this.pools$ = this.tournamentService.getAllPools(this.tournamentId, categoryId);
+    this.pools$ = this.tournamentService.getAllPools(
+      this.tournamentId,
+      categoryId
+    );
     this.hasFinales = hasFinales;
     this.loaderToggle.loaderInvisible();
   }
@@ -323,12 +359,13 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
   updateGameScore(game: any) {
     this.loaderToggle.loaderInvisible();
 
-    this.refreshData()
+    this.refreshData();
   }
 
-
   switchCategory(category: Category) {
+    console.log(this.myTournamentData);
     if (category != null) this.createBracket(category.id);
+    console.log(this.myTournamentData);
     for (let i = 0; i < this.categoryList.length; i++) {
       if (this.categoryList[i].id == category.id) {
         this.selectedCategoryIndex = i;
@@ -339,7 +376,4 @@ export class LiveScoreComponent implements OnInit, AfterViewInit {
   hideGameEdit() {
     this.editGame.hideEdit();
   }
-
-
-
 }
