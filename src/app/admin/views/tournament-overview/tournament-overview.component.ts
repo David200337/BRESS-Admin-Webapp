4cdd4f52fac6f65d6a1667c0c1a7c4f56fba745f
FSGabrsek <f.gabrsek@gmail.com>
2022-01-19 13:09:19 +0100

    fixed field count

--- src/app/admin/views/tournament-overview/tournament-overview.component.ts
+++ src/app/admin/views/tournament-overview/tournament-overview.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Subscription, switchMap, tap } from 'rxjs';
+import { Field } from 'src/app/models/field.model';
 import { Tournament } from 'src/app/models/tournament.model';
+import { FieldService } from 'src/app/services/field.service';
 import { LoaderToggleService } from 'src/app/services/loader-toggle.service';
 import { RpcService } from 'src/app/services/rpc.service';
 import { TournamentService } from 'src/app/services/tournament.service';
@@ -16,18 +18,30 @@ export class TournamentOverviewComponent implements OnInit {
   tournament: Tournament | undefined = undefined;
   sub!: Subscription
   canStart: boolean = true;
+  fields!: Field[];
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private tournamentService: TournamentService,
     private loaderToggle: LoaderToggleService,
-    private rpcService: RpcService
+    private rpcService: RpcService,
+    private fieldService: FieldService
   ) {
     loaderToggle.loaderVisible();
   }
 
   ngOnInit(): void {
+    this.fieldService.getList().subscribe({
+			next: (fields: any) => {
+				this.fields = fields;
+				console.log(fields);
+			},
+			error: (err) => {
+				// TODO: Handle error
+				console.log(err);
+			}
+		});
     this.sub = this.route.paramMap
       .pipe(
         switchMap((paramMap: ParamMap) => {
